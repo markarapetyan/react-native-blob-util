6488d40cd9498d1cd4a190b6314ab981d9a87cf8
Mariam <42065031+markarapetyan@users.noreply.github.com>
2025-07-17 16:20:01 +0400

    Update ReactNativeBlobUtilPackage.java

--- android/src/main/java/com/ReactNativeBlobUtil/ReactNativeBlobUtilPackage.java
+++ android/src/main/java/com/ReactNativeBlobUtil/ReactNativeBlobUtilPackage.java
@@ -1,48 +1,25 @@
 package com.ReactNativeBlobUtil;
 
-import androidx.annotation.Nullable;
-
 import com.facebook.react.ReactPackage;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
-import com.facebook.react.module.model.ReactModuleInfo;
-import com.facebook.react.module.model.ReactModuleInfoProvider;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
-// trick autolinking till it is fixed on RN side
-//public class ReactNativeBlobUtilPackage extends TurboReactPackage {
 public class ReactNativeBlobUtilPackage implements ReactPackage {
 
-    @Nullable
-    @Override
-    public NativeModule getModule(String name, ReactApplicationContext reactContext) {
-        if (name.equals(ReactNativeBlobUtilImpl.NAME)) {
-            return new ReactNativeBlobUtil(reactContext);
-        } else {
-            return null;
-        }
-    }
+  @Override
+  public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+    List<NativeModule> modules = new ArrayList<>();
+    modules.add(new ReactNativeBlobUtil(reactContext));
+    return modules;
+  }
 
-    @Override
-    public ReactModuleInfoProvider getReactModuleInfoProvider() {
-        return () -> {
-            final Map<String, ReactModuleInfo> moduleInfos = new HashMap<>();
-            boolean isTurboModule = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
-            moduleInfos.put(
-                    ReactNativeBlobUtilImpl.NAME,
-                    new ReactModuleInfo(
-                            ReactNativeBlobUtilImpl.NAME,
-                            ReactNativeBlobUtilImpl.NAME,
-                            false, // canOverrideExistingModule
-                            false, // needsEagerInit
-                            false, // isCxxModule
-                            isTurboModule // isTurboModule
-                    ));
-            return moduleInfos;
-        };
-    }
-
+  @Override
+  public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+    return Collections.emptyList();
+  }
 }
